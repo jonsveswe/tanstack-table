3a8e174b6fd3719ac201acea0c14e2b6ef86ce98
jonassvensson <jonas.svensson@midsummer.se>
2022-07-22 11:17:54 +0200

    Three original tables. One stripped down filter table.

---
+++ src/Table4MyFilters.tsx
@@ -0,0 +1,226 @@
+import React, { FC } from "react";
+import "./App.css";
+import "./table.css";
+
+import {
+  Column,
+  Table,
+  useReactTable,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getSortedRowModel,
+  FilterFn,
+  ColumnDef,
+  flexRender,
+} from "@tanstack/react-table";
+
+import {
+  RankingInfo,
+  rankItem,
+  compareItems,
+} from "@tanstack/match-sorter-utils";
+
+import { makeData, Person } from "./makeData";
+
+/* declare module "@tanstack/table-core" {
+  interface FilterMeta {
+    itemRank: RankingInfo;
+  }
+} */
+
+const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
+  // Rank the item
+  const itemRank = rankItem(row.getValue(columnId), value);
+
+  // Store the itemRank info
+  addMeta({
+    itemRank,
+  });
+  console.log("itemRank");
+  console.log(itemRank);
+  // Return if the item should be filtered in/out
+  return itemRank.passed;
+};
+
+function Table4MyFilters() {
+  const rerender = React.useReducer(() => ({}), {})[1];
+
+  const [globalFilter, setGlobalFilter] = React.useState("");
+
+  const columns = React.useMemo<ColumnDef<Person>[]>(
+    () => [
+      {
+        accessorKey: "firstName",
+        cell: (info) => info.getValue(),
+        footer: (props) => props.column.id,
+      },
+      {
+        accessorFn: (row) => row.lastName,
+        id: "lastName",
+        cell: (info) => info.getValue(),
+        header: () => <span>Last Name</span>,
+        footer: (props) => props.column.id,
+      },
+      {
+        accessorFn: (row) => `${row.firstName} ${row.lastName}`,
+        id: "fullName",
+        header: "Full Name",
+        cell: (info) => info.getValue(),
+        footer: (props) => props.column.id,
+      },
+
+      {
+        accessorKey: "age",
+        header: () => "Age",
+        footer: (props) => props.column.id,
+      },
+      {
+        accessorKey: "visits",
+        header: () => <span>Visits</span>,
+        footer: (props) => props.column.id,
+      },
+      {
+        accessorKey: "status",
+        header: "Status",
+        footer: (props) => props.column.id,
+      },
+      {
+        accessorKey: "progress",
+        header: "Profile Progress",
+        footer: (props) => props.column.id,
+      },
+    ],
+    []
+  );
+
+  const [data, setData] = React.useState(() => makeData(15));
+  const refreshData = () => setData((old) => makeData(50));
+
+  const table = useReactTable({
+    data,
+    columns,
+    state: {
+      globalFilter,
+    },
+    onGlobalFilterChange: setGlobalFilter,
+    globalFilterFn: fuzzyFilter,
+    enableGlobalFilter: true,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    debugTable: true,
+    debugHeaders: true,
+    debugColumns: false,
+  });
+
+  return (
+    <div className="p-2">
+      <div>
+        <DebouncedInput
+          value={globalFilter ?? ""}
+          onChange={(value) => setGlobalFilter(String(value))}
+          className="p-2 font-lg shadow border border-block"
+          placeholder="Search table..."
+        />
+      </div>
+      <div className="h-2" />
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <th key={header.id} colSpan={header.colSpan}>
+                    {header.isPlaceholder ? null : (
+                      <>
+                        <div
+                          {...{
+                            className: header.column.getCanSort()
+                              ? "cursor-pointer select-none"
+                              : "",
+                            onClick: header.column.getToggleSortingHandler(),
+                          }}
+                        >
+                          {flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                          {{
+                            asc: " 🔼",
+                            desc: " 🔽",
+                          }[header.column.getIsSorted() as string] ?? null}
+                        </div>
+                      </>
+                    )}
+                  </th>
+                );
+              })}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => {
+            return (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => {
+                  return (
+                    <td key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div>{table.getPrePaginationRowModel().rows.length} Rows</div>
+      <div>
+        <button onClick={() => rerender()}>Force Rerender</button>
+      </div>
+      <div>
+        <button onClick={() => refreshData()}>Refresh Data</button>
+      </div>
+      <pre>{JSON.stringify(table.getState(), null, 2)}</pre>
+    </div>
+  );
+}
+
+// A debounced input react component
+function DebouncedInput({
+  value: initialValue,
+  onChange,
+  debounce = 500,
+  ...props
+}: {
+  value: string | number;
+  onChange: (value: string | number) => void;
+  debounce?: number;
+} & Omit<React.InputHTMLAttributes<HTMLInputElement>, "onChange">) {
+  const [value, setValue] = React.useState(initialValue);
+
+  React.useEffect(() => {
+    setValue(initialValue);
+  }, [initialValue]);
+
+  React.useEffect(() => {
+    const timeout = setTimeout(() => {
+      onChange(value);
+    }, debounce);
+
+    return () => clearTimeout(timeout);
+  }, [value]);
+
+  return (
+    <input
+      {...props}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
+}
+
+export default Table4MyFilters;
